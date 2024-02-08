4f0e5065c11c9bd45ac96868483840a9acf26339
root <pardhupardhu7@gmail.com>
2024-02-08 06:19:35 +0000

    Travel Guide

---
+++ src/Components/LocationContainer/index.js
@@ -0,0 +1,24 @@
+import {
+  LocationCardContainer,
+  Heading,
+  HeadingDescriptionContainer,
+  Paragraph,
+} from './styledComponents'
+
+const LocationContainer = props => {
+  const {locationData} = props
+  const {imageUrl, description, name} = locationData
+
+  return (
+    <li className="list-location">
+      <LocationCardContainer>
+        <img src={imageUrl} alt={name} className="image" />
+        <HeadingDescriptionContainer>
+          <Heading>{name}</Heading>
+          <Paragraph>{description}</Paragraph>
+        </HeadingDescriptionContainer>
+      </LocationCardContainer>
+    </li>
+  )
+}
+export default LocationContainer
